0f1b9dd2bdeb795f71c0065b11861bac939a0ab9
BillyGalbreath <blake.galbreath@gmail.com>
2021-12-26 19:42:31 -0600

    Simplify project

---
+++ src/main/java/my/plugin/TestPlugin.java
@@ -0,0 +1,10 @@
+package my.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TestPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        //
+    }
+}
